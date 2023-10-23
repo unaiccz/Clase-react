bbf7ce2af500d372b3ec09fb024a7ae5ccdf207b
luisnisc <luisnisc30mayo2005@gmail.com>
2023-10-23 14:06:37 +0200

    .

--- src/Examen.jsx
+++ src/Examen.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { pos } from "./components/Ft";
 import View from "./components/View";
 import Swal from "sweetalert2";
-
+import "./App.css"
 function Examen() {
   const rl = () => window.location.reload();
 
@@ -16,8 +16,29 @@ function Examen() {
   });
 
   return (
-    <div>
-      <h1>Examenes</h1>
+<>
+<form onSubmit={hs}>
+  <div className="mb-6 ">
+    <label for="asignatura" class="block ml-4 mb-1 mt-1 text-sm font-medium text-white dark:text-white">Asignatura: </label>
+    <input type="text" id="asignatura" className="ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Asignatura" {...register("asignature")} required/>
+  </div>
+  <div className="mb-6">
+    <label for="fecha" className="block ml-4 mb-2 text-sm font-medium text-white dark:text-white">Fecha: </label>
+    <input type="text" id="fecha" className="ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Fecha" {...register("date")} required/>
+  </div>
+  <div className="flex items-start mb-6">
+  </div>
+  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2" onClick={()=>   Swal.fire('examen añadido, porfavor reinicia')}>Crear</button>
+  <button onClick={rl} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2">Reiniciar</button>
+</form>
+  <View/>
+  </>
+  );
+}
+
+export default Examen;
+{/* <div>
+      <h1 className="text-black">Examenes</h1>
           <form onSubmit={hs}>
        <div className="mb-3 mx-5">
        <input
@@ -32,8 +53,4 @@ function Examen() {
        </div>
       </form>
       <View/>
-    </div>
-  );
-}
-
-export default Examen;
+    </div> */}
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+
 function View() {
-  const del = (id,) => {
-axios.delete(`http://localhost:8000/clase/api/api/${id}/`)
-Swal.fire(`item ${id} deleted`)
-  }
   const [ex, setEx] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await fetch('http://localhost:8000/clase/api/api/');
-        if (!res.ok) {
-          throw new Error('Error en la solicitud a la API');
-        }
-        const data = await res.json();
-        setEx(data);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/clase/api/api/');
+      setEx(response.data);
+      setLoading(false);
+    } catch (error) {
+      setError('Error fetching data from API: ' + error.message);
+      setLoading(false);
     }
+  };
 
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8000/clase/api/api/${id}/`);
+      setEx((prevEx) => prevEx.filter((item) => item.id !== id));
+      Swal.fire(`Item ${id} deleted`);
+    } catch (error) {
+      Swal.fire('Error deleting item: ' + error.message);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
-  }, []);
+  }, []); // Empty dependency array means this effect will only run once after the initial render
 
   if (loading) {
     return <div>Cargando...</div>;
@@ -39,23 +42,28 @@ Swal.fire(`item ${id} deleted`)
 
   return (
     <div>
-      <h3>Examenes venideros</h3>
+      <h3 className='text-center mb-1 mt-1'>Examenes</h3>
       <hr />
-      <ul>
-        {ex.map((item, index) => (
-<div key={index} className='card'
-//estilos de caja 
->
-  <p>id: {item.id}</p>
-<p className='card-title'>Asignatura: <br /> {item.asignature}</p>
-<p className='card-text'>Fecha: {item.date}</p>
-<button className='btn btn-danger w-5' onClick={()=> del(item.id)}>DELETE</button>
-</div>
-
-))}
+      <div className='flex flex-row min-h-screen justify-center items-center'>
+      <ul className=''>
+        {ex.map((item) => (
+          <div key={item.id} className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-10'>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{item.id-62}</h5>
+            <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+              Asignatura: {item.asignature} <br />
+              Fecha: {item.date}
+            </p>
+            <button className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900' onClick={() => handleDelete(item.id)}>
+              DELETE
+            </button>
+          </div>
+        ))}
       </ul>
+      </div>
     </div>
   );
 }
 
 export default View;
+
+
